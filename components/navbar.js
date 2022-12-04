d4cfebf3677e94daec0c26fd5087c3dea6d2c65f
haydend01 <76508648+haydend01@users.noreply.github.com>
2022-12-04 16:49:04 +0000

    Deleted navbar buttons for download and pro version.

--- components/navbar.js
+++ components/navbar.js
@@ -28,16 +28,10 @@ export default function Navbar(props) {
       href: "/archive"
     },
     {
-      label: "Pro Version",
-      href: "https://stablo-pro.web3templates.com/",
-      external: true,
-      badge: "new"
-    },
-    {
-      label: "Download",
-      href: "https://web3templates.com/templates/stablo-minimal-blog-website-template",
-      external: true
+      label: "Contact",
+      href: "/contact"
     }
+    
   ];
 
   const mobilemenu = [...leftmenu, ...rightmenu];
